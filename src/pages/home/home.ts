2fbf0837e6b1ee06eaa398f5de44fafcdee7f02a
Shoichi Nonomura <nonoichi123@gmail.com>
2018-10-20 22:09:01 +0900

    一覧の商品制御を追加

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ActionSheetController } from 'ionic-angular';
 import { AddStockPage } from '../add-stock/add-stock';
 
 @Component({
@@ -10,11 +11,46 @@ export class HomePage {
 
   public items: string[] = ['キャベツ', 'りんご', 'みかん', 'ピーマン'];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public actionSheetCtrl: ActionSheetController) {
 
   }
 
   addStock() {
     this.navCtrl.push('AddStockPage');
+  }
+
+  itemSelected(item) {
+    const actionSheet = this.actionSheetCtrl.create({
+      title: item + ' をどうしますか？',
+      buttons: [
+        {
+          text: '空になった',
+          handler: () => {
+            console.log('空になった');
+
+          }
+        }, {
+          text: 'もう買わない',
+          // role: 'destructive',
+          handler: () => {
+            console.log('もう買わない');
+          }
+        }, {
+          text: '内容をかえたい',
+          // role: 'destructive',
+          handler: () => {
+            console.log('内容を変える');
+            this.navCtrl.push('AddStockPage');
+          }
+        }, {
+          text: 'なにもしない',
+          // role: 'cancel',
+          handler: () => {
+            console.log('なにもしない');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
   }
 }
