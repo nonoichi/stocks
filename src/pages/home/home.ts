35f2f3e08f7473bafafe9b9594252d8bfb9220ba
Shoichi Nonomura <nonoichi123@gmail.com>
2018-10-20 19:50:16 +0900

    add-stock ページへの遷移処理追加

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AddStockPage } from '../add-stock/add-stock';
 
 @Component({
   selector: 'page-home',
@@ -11,4 +12,7 @@ export class HomePage {
 
   }
 
+  addStock() {
+    this.navCtrl.push('AddStockPage');
+  }
 }
